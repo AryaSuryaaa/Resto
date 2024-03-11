4a9218d7fe53991168497280cadd015ef5274967
AryaSurya <suryasaputra914@gmail.com>
2024-03-11 16:00:09 +0700

    membuat domain model supaya model tidak tergantung dengan library Room.

--- app/src/main/java/com/aryasurya/gamebro/detail/DetailTourismViewModel.kt
+++ app/src/main/java/com/aryasurya/gamebro/detail/DetailTourismViewModel.kt
@@ -3,8 +3,9 @@ package com.aryasurya.gamebro.detail
 import androidx.lifecycle.ViewModel
 import com.aryasurya.gamebro.core.data.TourismRepository
 import com.aryasurya.gamebro.core.data.source.local.entity.TourismEntity
+import com.aryasurya.gamebro.core.domain.model.Tourism
 
 class DetailTourismViewModel(private val tourismRepository: TourismRepository) : ViewModel() {
-    fun setFavoriteTourism(tourism: TourismEntity, newStatus:Boolean) = tourismRepository.setFavoriteTourism(tourism, newStatus)
+    fun setFavoriteTourism(tourism: Tourism, newStatus:Boolean) = tourismRepository.setFavoriteTourism(tourism, newStatus)
 }
 
