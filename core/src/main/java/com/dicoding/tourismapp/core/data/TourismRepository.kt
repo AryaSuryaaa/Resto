998059bac6095aea7676bb74a28761c2a277b965
AryaSurya <suryasaputra914@gmail.com>
2024-03-14 14:26:43 +0700

    Menerapkan modularization

--- core/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ core/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -20,8 +20,8 @@ class TourismRepository @Inject constructor(
     private val appExecutors: AppExecutors
 ) : ITourismRepository {
 
-    override fun getAllTourism(): Flow<Resource<List<Tourism>>> =
-        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>() {
+    override fun getAllTourism(): Flow<com.dicoding.tourismapp.core.data.Resource<List<Tourism>>> =
+        object : com.dicoding.tourismapp.core.data.NetworkBoundResource<List<Tourism>, List<TourismResponse>>() {
             override fun loadFromDB(): Flow<List<Tourism>> {
                 return localDataSource.getAllTourism().map { DataMapper.mapEntitiesToDomain(it) }
             }
